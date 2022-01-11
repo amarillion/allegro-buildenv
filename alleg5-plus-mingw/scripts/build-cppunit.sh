4bb5444daa1b5c97b4ad9101746d768f32cb6e75
Martijn van Iersel <mvaniersel@gmail.com>
2022-01-11 07:36:48 +0100

    plus,mingw: add cppunit

---
+++ alleg5-plus-mingw/scripts/build-cppunit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+wget https://sourceforge.net/projects/cppunit/files/cppunit/1.12.1/cppunit-1.12.1.tar.gz/download -O- | tar xfz -
+pushd cppunit*
+./configure \
+    --host=i686-w64-mingw32 \
+    --prefix=/usr/i686-w64-mingw32 \
+    CPPFLAGS="-I/usr/i686-w64-mingw32/include" \
+    LDFLAGS="-L/usr/i686-w64-mingw32/lib"
+make
+sudo make install
+popd
